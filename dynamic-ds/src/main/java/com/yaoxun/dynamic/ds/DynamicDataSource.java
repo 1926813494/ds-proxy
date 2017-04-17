5653e9ed35f232e5b80375bc36d0c5463aa3e6b3
Loren <Loren@qq.com>
2017-04-17 23:56:43 +0800

    初始化工程

---
+++ dynamic-ds/src/main/java/com/yaoxun/dynamic/ds/DynamicDataSource.java
@@ -0,0 +1,12 @@
+package com.yaoxun.dynamic.ds;
+
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+
+public class DynamicDataSource extends AbstractRoutingDataSource {
+
+	@Override
+	protected Object determineCurrentLookupKey() {
+		return null;
+	}
+
+}
